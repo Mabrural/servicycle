f8ab84e908cee995b421cef1b34b6f2fa6c0cea0
mabrural <mabruralmutaqi@gmail.com>
2025-09-17 19:20:12 +0700

    perubahan floating upgrade ke premium agar tidak muncul

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -38,10 +38,10 @@
     </div>
     <!-- / Layout wrapper -->
 
-    <div class="buy-now">
+    {{-- <div class="buy-now">
         <a href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/" target="_blank"
             class="btn btn-danger btn-buy-now">Upgrade ke Premium</a>
-    </div>
+    </div> --}}
 
     @include('layouts.script')
 </body>
