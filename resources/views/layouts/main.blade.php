78e18d85f81666f689d9a1c514eb0bd91f992cc0
mabrural <mabruralmutaqi@gmail.com>
2025-09-17 14:51:02 +0700

    perubahan pada main.blade.php dan sidebar,blade.php sementara

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -40,7 +40,7 @@
 
     <div class="buy-now">
         <a href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/" target="_blank"
-            class="btn btn-danger btn-buy-now">Upgrade to Pro</a>
+            class="btn btn-danger btn-buy-now">Upgrade ke Premium</a>
     </div>
 
     @include('layouts.script')
