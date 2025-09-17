49da2e1d90796fd2a55c5823704521255b48bd2e
mabrural <mabruralmutaqi@gmail.com>
2025-09-17 14:11:53 +0700

    templating dashboard/index sudah jadi namun kontennya belum

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,9 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('layouts.main');
+    return view('dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
