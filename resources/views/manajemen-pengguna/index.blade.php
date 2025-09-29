ddd2601be7119d452865847b8fd2848bd3109cf0
mabrural <mabruralmutaqi@gmail.com>
2025-09-29 20:00:13 +0700

    perubahan pada footer

--- resources/views/manajemen-pengguna/index.blade.php
+++ resources/views/manajemen-pengguna/index.blade.php
@@ -3,12 +3,11 @@
 @section('container')
 <div class="container-xxl flex-grow-1 container-p-y">
     <h4 class="fw-bold py-3 mb-4">
-        <span class="text-muted fw-light">Dashboard /</span> Manajemen Pengguna
+        Manajemen Pengguna
     </h4>
 
     <!-- Hoverable Table rows -->
     <div class="card">
-        <h5 class="card-header">Manajemen Pengguna</h5>
         <div class="table-responsive text-nowrap">
             <table class="table table-hover table-sm">
                 <thead>
