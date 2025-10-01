2c4ad397e61326962c8a24a2d722cc337031396f
mabrural <mabruralmutaqi@gmail.com>
2025-10-01 18:40:38 +0700

    perubahan edit profile agar tampilan lebih elegan

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
-<html
-  lang="en"
-  class="light-style customizer-hide"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="{{ asset('') }}"
-  data-template="vertical-menu-template-free"
->
-  <head>
+<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default"
+    data-assets-path="{{ asset('') }}" data-template="vertical-menu-template-free">
+
+<head>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
-    />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>Profile | ServiCycle</title>
 
@@ -22,10 +15,8 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700&display=swap"
-      rel="stylesheet"
-    />
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet" />
 
     <!-- Icons -->
     <link rel="stylesheet" href="{{ asset('vendor/fonts/boxicons.css') }}" />
@@ -41,15 +32,21 @@
     <!-- Helpers -->
     <script src="{{ asset('vendor/js/helpers.js') }}"></script>
     <script src="{{ asset('js/config.js') }}"></script>
-  </head>
+</head>
 
-  <body>
-    <div class="container-xxl flex-grow-1 container-p-y">
-        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Akun /</span> Profile</h4>
-        <br>
-        <a href="{{ url('/dashboard') }}" class="btn btn-secondary mb-2">
-                <i class="bx bx-arrow-back me-1"></i> Kembali ke Dashboard
-            </a>
+<body>
+    <!-- Logo -->
+    <div class="app-brand justify-content-center mt-5">
+        <a href="{{ route('login') }}" class="app-brand-link gap-2">
+            <span class="app-brand-logo demo">
+                <img src="{{ asset('img/favicon/favicon.ico') }}" alt="">
+            </span>
+            <span class="app-brand-text demo text-body fw-bolder">ServiCycle</span>
+        </a>
+    </div>
+    <!-- /Logo -->
+    <div class="container-xxl col-lg-6 flex-grow-1 container-p-y">
+
         <!-- Update Profile Information -->
         <div class="card mb-4">
             <div class="card-header">
@@ -62,17 +59,19 @@
 
                     <div class="mb-3">
                         <label class="form-label" for="name">Nama</label>
-                        <input id="name" type="text" class="form-control" name="name" value="{{ old('name', auth()->user()->name) }}" required autofocus autocomplete="name" />
+                        <input id="name" type="text" class="form-control" name="name"
+                            value="{{ old('name', auth()->user()->name) }}" required autofocus autocomplete="name" />
                         @error('name')
-                          <div class="text-danger mt-1">{{ $message }}</div>
+                            <div class="text-danger mt-1">{{ $message }}</div>
                         @enderror
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label" for="email">Email</label>
-                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email', auth()->user()->email) }}" required autocomplete="username" />
+                        <input id="email" type="email" class="form-control" name="email"
+                            value="{{ old('email', auth()->user()->email) }}" required autocomplete="username" />
                         @error('email')
-                          <div class="text-danger mt-1">{{ $message }}</div>
+                            <div class="text-danger mt-1">{{ $message }}</div>
                         @enderror
                     </div>
 
@@ -95,23 +94,26 @@
 
                     <div class="mb-3">
                         <label class="form-label" for="current_password">Kata Sandi Saat Ini</label>
-                        <input id="current_password" type="password" class="form-control" name="current_password" required autocomplete="current-password" />
+                        <input id="current_password" type="password" class="form-control" name="current_password"
+                            required autocomplete="current-password" />
                         @error('current_password')
-                          <div class="text-danger mt-1">{{ $message }}</div>
+                            <div class="text-danger mt-1">{{ $message }}</div>
                         @enderror
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label" for="password">Kata Sandi Baru</label>
-                        <input id="password" type="password" class="form-control" name="password" required autocomplete="new-password" />
+                        <input id="password" type="password" class="form-control" name="password" required
+                            autocomplete="new-password" />
                         @error('password')
-                          <div class="text-danger mt-1">{{ $message }}</div>
+                            <div class="text-danger mt-1">{{ $message }}</div>
                         @enderror
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label" for="password_confirmation">Konfirmasi Kata Sandi Baru</label>
-                        <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" />
+                        <input id="password_confirmation" type="password" class="form-control"
+                            name="password_confirmation" required autocomplete="new-password" />
                     </div>
 
                     <div class="mb-3">
@@ -127,16 +129,19 @@
                 <h5 class="mb-0 text-danger">Hapus Akun</h5>
             </div>
             <div class="card-body">
-                <p class="mb-3">Begitu akun Anda dihapus, semua sumber daya dan data yang terkait akan dihapus secara permanen. Silakan unduh data atau informasi apa pun yang ingin Anda simpan sebelum menghapus akun Anda.</p>
+                <p class="mb-3">Begitu akun Anda dihapus, semua sumber daya dan data yang terkait akan dihapus secara
+                    permanen. Silakan unduh data atau informasi apa pun yang ingin Anda simpan sebelum menghapus akun
+                    Anda.</p>
                 <form method="POST" action="{{ route('profile.destroy') }}">
                     @csrf
                     @method('DELETE')
 
                     <div class="mb-3">
                         <label class="form-label" for="password_delete">Kata Sandi</label>
-                        <input id="password_delete" type="password" class="form-control" name="password" required placeholder="Masukkan kata sandi untuk konfirmasi" />
+                        <input id="password_delete" type="password" class="form-control" name="password" required
+                            placeholder="Masukkan kata sandi untuk konfirmasi" />
                         @error('password')
-                          <div class="text-danger mt-1">{{ $message }}</div>
+                            <div class="text-danger mt-1">{{ $message }}</div>
                         @enderror
                     </div>
 
@@ -147,7 +152,12 @@
             </div>
         </div>
     </div>
-
+    <center>
+        <a href="{{ url('/dashboard') }}" class="btn btn-outline-secondary mb-2">
+            <i class="bx bx-arrow-back me-1"></i> Kembali ke Dashboard
+        </a>
+    </center>
+    <br><br>
     <!-- Core JS -->
     <script src="{{ asset('vendor/libs/jquery/jquery.js') }}"></script>
     <script src="{{ asset('vendor/libs/popper/popper.js') }}"></script>
@@ -156,5 +166,6 @@
 
     <script src="{{ asset('vendor/js/menu.js') }}"></script>
     <script src="{{ asset('js/main.js') }}"></script>
-  </body>
+</body>
+
 </html>
