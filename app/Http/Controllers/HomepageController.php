4816678635b3b87419036f412ec66de17a79ce87
mabrural <mabruralmutaqi@gmail.com>
2025-10-20 14:43:44 +0700

    pembuatan formula untuk menampilkan bengkel terdekat beserta jaraknya sudah berhasil dibuat di basic.blade.php

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Workshop;
 use Illuminate\Http\Request;
 
 class HomepageController extends Controller
 {
+    // public function index()
+    // {
+    //     return view('homepage.index');
+    // }
     public function index()
-    {
-        return view('homepage.index');
-    }
+{
+    $workshops = Workshop::select('id', 'name', 'address', 'latitude', 'longitude')->get();
+    return view('homepage.index', compact('workshops'));
+}
+
 }
